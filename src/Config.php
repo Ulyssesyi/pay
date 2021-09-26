5372beae24c41792e91f5f09a9808bdbd163d1ca
yijin <yijin@91laiqian.com>
2021-09-26 10:09:20 +0800

    feat: 文档完善

--- src/Config.php
+++ src/Config.php
@@ -17,10 +17,9 @@ namespace Yijin\Pay;
  * @property string $appid 微信支付时发起支付的公众号/小程序的APP ID/支付宝直连的APPID
  * @property int $isMiniProgram webPay是不是由小程序发起，1-小程序，0-公众号/服务窗/js支付
  * @property string $userId 用户在微信/支付宝中的id，即微信的openid，支付宝的buyer_id .etc
- * @property int $expireTime 订单有效截止10位（秒级）时间戳
  * @property string $userIP 请求IP
  *
- * 支付宝官方配置
+ * 支付宝直连参数
  * @property string $appAuthToken ISV服务商模式下的授权token
  * @property string $merchantPrivateKey 应用私钥，例如：MIIEvQIBADANB
  * @property string $alipayCertPath 支付宝公钥证书文件路径，例如：/foo/alipayCertPublicKey_RSA2.crt
@@ -29,7 +28,7 @@ namespace Yijin\Pay;
  * @property string $alipayPublicKey 支付宝公钥，例如：MIIBIjANBg，非证书模式，填写这个公钥即可，上面三个证书可以不填写
  * @property string $encryptKey AES密钥，调用AES加解密相关接口时需要，非必填
  *
- * 微信官方配置
+ * 微信直连参数
  * @property string $mchId 商户号
  * @property string $subAppId 子商户的公众号/小程序的APP ID
  * @property string $subMchId 子商户号
@@ -37,34 +36,34 @@ namespace Yijin\Pay;
  * @property string $clientApiV2KeyFilePath 商户API v2证书
  * @property string $clientApiV2CertFilePath 商户API v2证书密钥
  * @property string $attach 附加数据，不建议使用
+ * @property int $expireTime 订单有效截止10位（秒级）时间戳
  *
- * 随行付配置
- * @property string $orgIdSxf 服务商机构编号
+ * 随行付参数
+ * @property string $orgIdSxf 服务商编号
  * @property string $merchantNoSxf 商户编号
- * @property string $orgPrivateRSAKeySxf 服务商机构RSA私钥内容
+ * @property string $orgPrivateRSAKeySxf 服务商RSA私钥内容
  * @property string $orgPublicRSAKeySxf 平台RSA公钥内容
  * @property string $outFrontUrlSxf H5支付后跳转网页地址
  * @property string $wechatFoodOrderSxf 微信扫码点餐标识，最大长度32位,目前需上送：FoodOrder
  * @property string $refundReasonSxf 退款原因。默认值：商家与消费者协商一致
  *
- * 收钱吧配置
+ * 收钱吧参数
  * @property string $serviceProviderIDSqb 服务商ID
  * @property string $activateCodeSqb 激活码
  * @property string $activateDeviceIDSqb 激活设备ID
  * @property string $terminalSNSqb 终端账号
  * @property string $terminalKeySqb 终端密钥
- * @property string $deviceIdSqb 设备唯一ID
  * @property string $operatorSqb 操作员
  * @property string $returnUrlSqb web支付后的跳回地址
  * @property string $reflectSqb web支付后的反射参数
  *
- * 付呗配置
+ * 付呗参数
  * @property string $merchantIdFb 商户ID
  * @property string $merchantKeyFb 商户密码
  * @property string $storeIdFb 商户门店ID
  * @property string $wxOpenIDFb 付呗网页下的用户ID
  *
- * 联拓配置
+ * 联拓参数
  * @property string $userNameLT 商户后台登录账号
  * @property string $userPwdLt 商户后台登录密码
  * @property string $appIdLt 合作方ID
@@ -72,12 +71,12 @@ namespace Yijin\Pay;
  * @property string $merchantCodeLt 商户编号
  * @property string $refundReasonLt 退款原因。默认值：商家与消费者协商一致
  *
- * 乐刷配置
+ * 乐刷参数
  * @property string $merchantIdLS 商户ID
  * @property string $serviceProviderKeyLS 服务商密码
  * @property string $jumpUrlLS 使用乐刷收银台支付后跳回地址
  *
- * 乐天成支付配置
+ * 乐天成支付参数
  * @property string $appKeyLtc 商户在乐天成的支付PayCode
  * @property string $accessSecretLtc 商户在乐天成的内容密钥
  * @property string $privateSecretLtc 商户在乐天成的私钥
